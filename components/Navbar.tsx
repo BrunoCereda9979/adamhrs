1a96aee7e9e60c2186786c16e8523a1d6d31001c
BrunoCereda9979 <bruno9979@gmail.com>
2024-08-30 01:57:12 -0400

    added absolute path to page logo againnnnnn

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
           <span className="mr-[3.5px] text-green-90">Adam</span>
           <span className="font-bold text-xl text-blue-90">HRS</span>
         </p>
-        <Image src="../public/quill.png" alt="Logo" width={26} height={25} />
+        <Image src="./public/quill.png" alt="Logo" width={26} height={25} />
       </Link>
       <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
